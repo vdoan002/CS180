d186c91ca3a675c3a8d02386f6b819049acdea91
Michael Chen <mchen046@ucr.edu>
2016-04-24 17:38:05 -0700

    added iOSApp/webView

---
+++ iOSApp/webView/UCR-Craigslist-webView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UCR-Craigslist-webView
+//
+//  Created by Michael Chen on 4/24/16.
+//  Copyright © 2016 UCR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
